30f0fdc82be79318027de75d935efabcb44e5421
ShadowLancer42 <57329674+ShadowLancer42@users.noreply.github.com>
2021-02-24 21:55:42 -0800

    Added Stamina System
    Added stamina system:
        - Added a slider that is controlled by stamina values
        - Stamina decreases when you're running, and replenishes when you aren't.
        - If you use up all of your stamina, you will become exhausted until your stamina is full. When exhausted you will replenish stamina slower, and be unable to run.
    
    Also added some spacers on the inspector variables in the ThirdPersonMovement script.

--- Assets/Scripts/Base Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/Base Scripts/ThirdPersonMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
@@ -10,23 +11,36 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform cam;
 
     private float speed;
+    [Space(10)]
     public float walkSpeed = 6f;
     public float runSpeed = 12f;
+    [Space(10)]
     public float stamina = 100;
     public float currentStamina;
     private bool healStamina = true;
-    public float staminaHealSpeed = 1;
+    private bool exausted = false;
+    [Space(5)]
+    public Color normalStamina;
+    public Color exaustedStamina;
+    public Color exaustedEmpty;
+    [Space(10)]
+    public float staminaUseSpeed = 1;
+    public float staminaHealSpeed = 3;
+    public float exaustedHealSpeed = 2;
+    public Slider staminaDisplay;
+    [Space(10)]
 
+    [Space(20)]
     public float gravity = -9.81f;
     public float yVelocityReset = -2f;
-
+    [Space(10)]
     public float jump = 10f;
     public float killFallDist = 30f;
-
+    [Space(10)]
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-    
+    [Space(10)]
     Vector3 velocity;
     bool isGrounded;
 
@@ -42,23 +56,27 @@ public class ThirdPersonMovement : MonoBehaviour
     float startFall;
     float endFall;
     float fallDist;
-
+    [Space]
     public bool playerIsControllable = true;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
-        
+
         //lock da' cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        //stamina setup
+        staminaDisplay.maxValue = stamina;
+        currentStamina = stamina;
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+
 
         //reset all animations to false
         #region
@@ -134,12 +152,13 @@ public class ThirdPersonMovement : MonoBehaviour
         {
             moving = true;
             //sprint if you have stamina and press sprint
-            if (Input.GetButton("Sprint") && currentStamina > 0)
+            if (Input.GetButton("Sprint") && !exausted)
             {
                 speed = runSpeed;
                 bool running = true;
                 anim.SetBool("IsRunning", running);
                 healStamina = false;    //don't heal stamina whilest running    //this gets reset to true every loop of the update function
+                currentStamina -= staminaUseSpeed * Time.deltaTime;
             }
             //if you don't have stamina or aren't pressing sprint, just walk
             else
@@ -149,12 +168,31 @@ public class ThirdPersonMovement : MonoBehaviour
                 anim.SetBool("IsWalking", walking);
             }
         }
+        if (currentStamina <= 0)
+        {
+            exausted = true;
+        }
 
+        //heal stamina
         if (healStamina == true && currentStamina < stamina)
         {
-            currentStamina += staminaHealSpeed * Time.deltaTime;
-            Debug.Log(currentStamina);
+            //regular heal speed
+            if (!exausted)
+            {
+                currentStamina += staminaHealSpeed * Time.deltaTime;
+            }
+            //exausted heal speed
+            else
+            {
+                currentStamina += exaustedHealSpeed * Time.deltaTime;
+            }
+            //set exausted to false if you heal up all the way
+            if (currentStamina >= stamina)
+            {
+                exausted = false;
+            }
         }
+        staminaDisplay.value = currentStamina;
 
 
         //idle
